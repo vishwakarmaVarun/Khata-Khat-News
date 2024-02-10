c93dd7da512b59179324cd577ab42eeca361eb04
varun vishwakarma <varun@webwithvarun.com>
2024-02-10 23:49:19 +0530

    added spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import loading from './loadingSpinner.gif'
+
+export class Spinner extends Component {
+  render() {
+    return (
+      <div className='text-center'>
+        <img src={loading} alt="loading" style={{width: "40px"}} />
+      </div>
+    )
+  }
+}
+
+export default Spinner
